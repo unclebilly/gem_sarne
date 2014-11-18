53c0b98caa0bfc8ca8a8c20d0d742ae7e07f57eb
Kyle Marek-Spartz <kyle.marekspartz@govdelivery.com>
2014-11-18 15:59:50 -0600

    say gem sarne

--- lib/gem_sarne/cli.rb
+++ lib/gem_sarne/cli.rb
@@ -4,6 +4,7 @@ module GemSarne
       Dir[File.expand_path("../../media/*", __FILE__)].each do |file|
         Launchy.open(file)
       end
+      `say gem sarne`
     end
   end
 end
--- lib/gem_sarne/version.rb
+++ lib/gem_sarne/version.rb
@@ -1,3 +1,3 @@
 module GemSarne
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
