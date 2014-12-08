5b4b19e586c278c08b95fe3f6d976ba19a5cf160
Benjamin Ortega <ben.ortega@gmail.com>
2014-12-08 11:52:39 -0600

    GemSarne is a singleton, for there can only be one.

--- lib/gem_sarne.rb
+++ lib/gem_sarne.rb
@@ -1,7 +1,25 @@
 require "launchy"
 require "gem_sarne/version"
 require "gem_sarne/cli"
+require 'singleton'
 
-module GemSarne
-  
+class GemSarne
+  include Singleton # there is only one
+
+  def run
+    rulz
+    `say gem sarne`
+  end
+
+  def rulz
+    Dir[File.expand_path("../media/*", __FILE__)].each do |file|
+      Launchy.open(file)
+    end
+  end
+
+  def cool?
+    # of course, always
+    true
+  end
+
 end
